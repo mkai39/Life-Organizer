3613e6636377523788df530a93b4ee16587f4a6e
Xiao Qing Yu <yuuxiaoqing@gmail.com>
2020-01-18 19:24:27 -0800

    update files

---
+++ life_event.java
@@ -0,0 +1,15 @@
+public class life_event{
+	int date, month, year;
+	String event_description;
+	//img picture; //stretch goal
+	life_event(int date, int month, int year, String event_description){
+
+		this.date = date;
+		this.month = month;
+		this.year = year;
+		this.event_description = event_description;
+
+		//this.picture; //stretch goal
+	}
+	
+}
