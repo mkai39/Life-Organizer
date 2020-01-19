e970d21a56bee796ccc2602dfbfe708042ae175e
Emmanuel Butor <emmanuelbutor.mehs@gmail.com>
2020-01-18 22:26:14 -0800

    Updated Makefile and OCRClient.java: Makefile --> fixed class path; OCRClient.java --> set up imports

--- source/OCRClient.java
+++ source/OCRClient.java
@@ -1,12 +1,18 @@
-/**
- * This is the client that reads all of the .json file that was
- * outputted from the Azure OCR.
- */
+import java.io.FileNotFoundException; 
+import java.io.PrintWriter; 
+import java.util.LinkedHashMap; 
+import java.util.Map;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.*; 
+/**
+ * This is the client that reads all of the .json file that was outputted from
+ * the Azure OCR.
+ */
 public class OCRClient
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
-        
     }
 }
