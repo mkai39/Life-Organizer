d7c859baea095b7d5cc2484c6cfda9cc8cdb2c06
Xiao Qing Yu <yuuxiaoqing@gmail.com>
2020-01-18 18:03:18 -0800

    button links work, camera works

---
+++ LifeOrganizer/app/src/main/java/com/example/lifeorganizer/Import_photo.java
@@ -0,0 +1,15 @@
+package com.example.lifeorganizer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Import_photo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_import_photo);
+    }
+
+}
--- LifeOrganizer/app/src/main/java/com/example/lifeorganizer/MainActivity.java
+++ LifeOrganizer/app/src/main/java/com/example/lifeorganizer/MainActivity.java
@@ -16,12 +16,14 @@ import androidx.navigation.ui.NavigationUI;
 public class MainActivity extends AppCompatActivity {
 
     private Button calendar;
+    private Button import_photo;
+    private Button schedule;
+    private Button camera;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         calendar = (Button) findViewById(R.id.calendar);
-
         calendar.setOnClickListener(new View.OnClickListener() {
             @Override
 
@@ -30,11 +32,54 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+
+        import_photo = (Button) findViewById(R.id.Import);
+        import_photo.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View V){
+                openImport();
+            }
+        });
+
+        schedule = (Button) findViewById(R.id.schedule);
+        schedule.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View V){
+                openSchedule();
+            }
+        });
+
+
+        camera = (Button) findViewById(R.id.camera);
+        camera.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View V){
+                openCamera();
+            }
+        });
+
+
     }
 
     public void openCalendar(){
         Intent calendar_intent = new Intent(this,Calendar.class);
         startActivity(calendar_intent);
     }
+
+    public void openImport(){
+        Intent import_intent = new Intent(this,Import_photo.class);
+        startActivity(import_intent);
+    }
+
+    public void openSchedule(){
+        Intent schedule_intent = new Intent(this,schedule.class);
+        startActivity(schedule_intent);
+    }
+
+    public void openCamera(){
+        Intent camera_intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        startActivity(camera_intent);
+    }
+
 
 }
