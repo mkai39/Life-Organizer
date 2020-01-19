d413577baacdffc641522ef286c9d09ebd5ccee1
Alice Yu <aliceyu544@gmail.com>
2020-01-18 23:29:06 -0800

    fixed import

--- LifeOrganizer/app/src/main/java/com/example/lifeorganizer/MainActivity.java
+++ LifeOrganizer/app/src/main/java/com/example/lifeorganizer/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.lifeorganizer;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -19,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
     private Button import_photo;
     private Button schedule;
     private Button camera;
+    ImageView imageView;
+    Uri imageUri;
+    private static int PICK_IMAGE = 100;
    // private Button home;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,10 +41,12 @@ public class MainActivity extends AppCompatActivity {
         });
 
         import_photo = (Button) findViewById(R.id.Import);
+        imageView = (ImageView)findViewById(R.id.imageView);
         import_photo.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View V){
-                openImport();
+                //   openImport();
+                openGallery();
             }
         });
 
@@ -75,10 +83,10 @@ public class MainActivity extends AppCompatActivity {
         startActivity(calendar_intent);
     }
 
-    public void openImport(){
+/*    public void openImport(){
         Intent import_intent = new Intent(this,Import_photo.class);
         startActivity(import_intent);
-    }
+    } */
 
     public void openSchedule(){
         Intent schedule_intent = new Intent(this,schedule.class);
@@ -88,6 +96,20 @@ public class MainActivity extends AppCompatActivity {
     public void openCamera(){
         Intent camera_intent = new Intent("android.media.action.IMAGE_CAPTURE");
         startActivity(camera_intent);
+    }
+
+    private void openGallery() {
+        Intent photo_intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(photo_intent, PICK_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int request, int result, Intent data) {
+        super.onActivityResult(request, result, data);
+        if (result == RESULT_OK && request == PICK_IMAGE) {
+            imageUri = data.getData();
+            imageView.setImageURI(imageUri);
+        }
     }
 
     /*public void openHome(){
